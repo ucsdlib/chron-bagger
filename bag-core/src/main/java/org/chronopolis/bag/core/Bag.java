c3c5eec2c318c1ec8bb19606eb7d9f96fa0c905a
Michael Ritter <shake@umiacs.umd.edu>
2015-11-06 12:42:57 -0500

    Add getters + setters for Bags

--- bag-core/src/main/java/org/chronopolis/bag/core/Bag.java
+++ bag-core/src/main/java/org/chronopolis/bag/core/Bag.java
@@ -1,5 +1,6 @@
 package org.chronopolis.bag.core;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,12 +9,12 @@ import java.util.List;
  */
 public class Bag {
 
-    // Used as the base directory
+	// Used as the base directory
     private String name;
 
     // Some metadata
-    private long size;
-    private long numFiles;
+    private long size = 0;
+    private long numFiles = 0;
 
     // Tag files
     private List<TagFile> tags;
@@ -27,5 +28,107 @@ public class Bag {
 
     // Receipt after building
     private String receipt;
+
+    public String getName() {
+		return name;
+	}
+
+	public Bag setName(String name) {
+		this.name = name;
+        return this;
+	}
+
+	/**
+	 * Return the size of the bags payload directory
+	 *
+	 * @return
+     */
+	public long getSize() {
+		return size;
+	}
+
+	public Bag setSize(long size) {
+		this.size = size;
+        return this;
+	}
+
+	public long getNumFiles() {
+		return numFiles;
+	}
+
+	public Bag setNumFiles(long numFiles) {
+		this.numFiles = numFiles;
+        return this;
+	}
+
+	public List<TagFile> getTags() {
+		return tags;
+	}
+
+	public Bag setTags(List<TagFile> tags) {
+		this.tags = tags;
+        return this;
+	}
+
+	public Bag addTag(TagFile tag) {
+        if (tags == null) {
+            tags = new ArrayList<>(); 
+        }
+		this.tags.add(tag);
+        return this;
+	}
+    
+	public List<PayloadFile> getFiles() {
+		return files;
+	}
+
+	public Bag setFiles(List<PayloadFile> files) {
+		this.files = files;
+        return this;
+	}
+
+    public Bag addFile(PayloadFile file) { 
+        if (files == null) {
+            files = new ArrayList<>();
+        }
+
+        this.files.add(file);
+        this.numFiles++;
+        this.size += file.getSize();
+        return this;
+    }
+
+	public TagManifest getTagManifest() {
+		return tagManifest;
+	}
+
+	public Bag setTagManifest(TagManifest tagManifest) {
+		this.tagManifest = tagManifest;
+        return this;
+	}
+
+	public PayloadManifest getManifest() {
+		return manifest;
+	}
+
+	public Bag setManifest(PayloadManifest manifest) {
+		this.manifest = manifest;
+        return this;
+	}
+
+	public String getReceipt() {
+		return receipt;
+	}
+
+    /**
+     * Set a receipt for the bag to use after building has completed
+     *
+     * @param receipt
+     * @return
+     */
+	public Bag setReceipt(String receipt) {
+		this.receipt = receipt;
+        return this;
+	}
 
 }
