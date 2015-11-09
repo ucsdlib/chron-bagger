b8d14775e2c602ed550774273c041f0f9296ab90
Michael Ritter <shake@umiacs.umd.edu>
2015-11-09 16:52:25 -0500

    Add bag formatted name to digest enum

--- bag-core/src/main/java/org/chronopolis/bag/core/Digest.java
+++ bag-core/src/main/java/org/chronopolis/bag/core/Digest.java
@@ -8,16 +8,22 @@ import com.google.common.hash.Hashing;
  * Created by shake on 7/30/2015.
  */
 public enum Digest {
-    SHA_256(Hashing.sha256()),
-    MD5(Hashing.md5());
+    SHA_256(Hashing.sha256(), "sha256"),
+    MD5(Hashing.md5(), "md5");
 
     private final HashFunction hashFunction;
+    private final String bagFormattedName;
 
-    Digest(HashFunction hashFunction) {
+    Digest(HashFunction hashFunction, String bagFormattedName) {
         this.hashFunction = hashFunction;
+        this.bagFormattedName = bagFormattedName;
     }
 
     public HashFunction getHashFunction() {
         return hashFunction;
+    }
+
+    public String getBagFormattedName() {
+        return bagFormattedName;
     }
 }
