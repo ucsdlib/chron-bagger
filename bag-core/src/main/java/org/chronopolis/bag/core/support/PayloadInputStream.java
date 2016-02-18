f0b17a6f016238af3fc7df2da2641b05afd4e10c
Michael Ritter <shake@umiacs.umd.edu>
2016-02-18 13:20:11 -0500

    Add an InputStream for our PayloadManifest to use
    This pulls from an iterator rather than pushing everything into a PipedInputStream, lessening the burden on memory by a bit

---
+++ bag-core/src/main/java/org/chronopolis/bag/core/support/PayloadInputStream.java
@@ -0,0 +1,42 @@
+package org.chronopolis.bag.core.support;
+
+import org.chronopolis.bag.core.PayloadFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ *
+ * Created by shake on 2/16/16.
+ */
+public class PayloadInputStream extends InputStream {
+
+    @Override
+    public int read() throws IOException {
+        if (current == null || current.position() == current.limit()) {
+            PayloadFile f;
+            if (it.hasNext()) {
+                f = it.next();
+            } else {
+                return -1;
+            }
+
+            current = ByteBuffer.wrap(f.toString().getBytes());
+        }
+
+        return current.get();
+    }
+
+    ByteBuffer current;
+    Set<PayloadFile> files;
+    Iterator<PayloadFile> it;
+
+    public PayloadInputStream(Set<PayloadFile> files) {
+        this.files = files;
+        it = files.iterator();
+    }
+
+}
