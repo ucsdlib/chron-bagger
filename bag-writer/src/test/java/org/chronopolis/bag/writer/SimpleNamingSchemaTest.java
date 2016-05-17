5da1f4f3825d9128d328681c62deddfbccbddd8a
Michael Ritter <shake@umiacs.umd.edu>
2016-05-17 14:47:31 -0400

    Add tests for our Naming Schemas

--- bag-writer/src/test/java/org/chronopolis/bag/writer/SimpleNamingSchemaTest.java
+++ bag-writer/src/test/java/org/chronopolis/bag/writer/SimpleNamingSchemaTest.java
@@ -1,16 +1,23 @@
 package org.chronopolis.bag.writer;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 /**
+ *
  * Created by shake on 5/13/16.
  */
 public class SimpleNamingSchemaTest {
     @Test
     public void getName() throws Exception {
+        String name = "my-name";
+        SimpleNamingSchema schema = new SimpleNamingSchema(name);
 
+        String gen = schema.getName(0);
+        Assert.assertEquals(name, gen);
+
+        gen = schema.getName(1);
+        Assert.assertNotEquals(name, gen);
+        Assert.assertTrue(gen.endsWith("-1"));
     }
-
 }
--- bag-writer/src/test/java/org/chronopolis/bag/writer/UUIDNamingSchemaTest.java
+++ bag-writer/src/test/java/org/chronopolis/bag/writer/UUIDNamingSchemaTest.java
@@ -1,8 +1,22 @@
 package org.chronopolis.bag.writer;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
+ *
  * Created by shake on 5/13/16.
  */
 public class UUIDNamingSchemaTest {
+
+    @Test
+    public void getName() {
+        UUIDNamingSchema schema = new UUIDNamingSchema();
+        String name = schema.getName(0);
+        String other = schema.getName(1);
+
+        Assert.assertNotEquals(name, other);
+        Assert.assertFalse(other.endsWith("-0"));
+    }
 
 }
